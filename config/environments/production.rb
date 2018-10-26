bcf1541530a18b50baa513b6ce059ac52a788607
Benji Rothman <brothman7000@gmail.com>
2018-10-26 19:23:48 -0400

    Commented some production.rb back in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.web_socket_server_url = "wss://plaque.herokuapp.com/cable"
-  # config.action_cable.url = 'wss://plaque.herokuapp.com/cable'
+  config.web_socket_server_url = "wss://plaque.herokuapp.com/cable"
+  config.action_cable.url = 'wss://plaque.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://plaque.herokuapp.com', 'https://plaque.herokuapp.com', /http:\/\/plaque.herokuapp*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
