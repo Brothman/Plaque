d86db13099092221d1a49e9057bc9a88e6282826
Benji Rothman <brothman7000@gmail.com>
2018-10-29 12:17:00 -0400

    Basic Emoji Support. Styling needs to be done. Need to find a way to parse chosen Emoji into input

--- frontend/components/MessageCard.jsx
+++ frontend/components/MessageCard.jsx
@@ -3,14 +3,37 @@ import Message from './Message';
 import { connect } from 'react-redux';
 import { receiveMessage } from '../actions/MessageActions';
 
+import EmojiPicker from 'emoji-picker-react';
+import JSEMOJI from 'emoji-js';
+
 class MessageCard extends React.Component {
     constructor(props) {
         super(props);
+        
         this.state = { 
             message: "",
+            jsemoji: this.setupJSEMOJI(),
          };
         this.sendMessage = this.sendMessage.bind(this);
         this.updateMessage = this.updateMessage.bind(this);
+        this.showEmojiPicker = this.showEmojiPicker.bind(this);
+        this.addEmoji = this.addEmoji.bind(this);
+    }
+
+    setupJSEMOJI() {
+        // new instance
+        const jsemoji = new JSEMOJI();
+        // set the style to emojione (default - apple)
+        jsemoji.img_set = 'emojione';
+        // set the storage location for all emojis
+        jsemoji.img_sets.emojione.path = 'https://cdn.jsdelivr.net/emojione/assets/3.0/png/32/';
+    
+        // some more settings...
+        jsemoji.supports_css = false;
+        jsemoji.allow_native = false;
+        jsemoji.replace_mode = 'unified';
+
+        return jsemoji;
     }
 
     componentDidMount(){
@@ -51,15 +74,41 @@ class MessageCard extends React.Component {
         });
     }
 
+    showEmojiPicker() {
+        // this.setState({ emojisVisible: true });
+        const myEmojiPicker = document.querySelector(".my-emoji-picker");
+        myEmojiPicker.style.visibility = "visible";
+    }
+
+    addEmoji(emoji, emojiData) {
+        const oldMessage = this.state.message;
+
+        const parsedEmoji = this.state.jsemoji.replace_colons(`:${emojiData.name}:`);
+        console.log(parsedEmoji);
+        // console.log(emoji)
+        // console.log(emojiData);
+        const message = oldMessage + parsedEmoji;
+        this.setState({ message });
+        const myEmojiPicker = document.querySelector(".my-emoji-picker");
+        myEmojiPicker.style.visibility = "hidden";
+    }
+
     render() { 
         const messages = this.props.messages.messageArray;
+
         return ( 
             <div className="message-card">
                 <div className="message-container">
                     {this.createMessages(messages)}
                 </div>
                 <form onSubmit={this.sendMessage}>
-                    
+
+                    <div className="my-emoji-picker" style={{ visibility: "hidden" }}>
+                        <EmojiPicker className="hello" onEmojiClick={this.addEmoji} />
+                    </div>
+
+                    <i onClick={this.showEmojiPicker} className="smile-icon far fa-smile"></i>
+           
                     <input  autoFocus={true} type="text"
                             onChange={this.updateMessage} 
                             className="message-input"
